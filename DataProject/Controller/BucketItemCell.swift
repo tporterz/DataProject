98e0a6b8482bf10896238169e5fe8c47f9f2f8f2
tporterz <gamingintensifys@gmail.com>
2020-01-08 07:33:12 -0700

    Created app from scratch after losing files due to new computer

---
+++ DataProject/Controller/BucketItemCell.swift
@@ -0,0 +1,68 @@
+//
+//  BucketItemCell.swift
+//  DataProject
+//
+//  Created by Porter, Tyler on 12/5/19.
+//  Copyright © 2019 CTEC. All rights reserved.
+//
+
+import UIKit
+
+class BucketItemCell: UITableViewCell
+{
+    @IBOutlet weak var bucketItemText : UILabel!
+    @IBOutlet weak var bucketItemSymbol : UILabel!
+    @IBOutlet weak var bucketItemSignature : UILabel!
+
+    override func awakeFromNib()
+    {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool)
+    {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+    var currentBucketItem : BucketItem!
+    {
+        didSet
+        {
+            updateCellView()
+        }
+    }
+    
+    private func randomEmoji() -> String
+    {
+        let emojiStart = 0x1F601
+        let emojiEnd = 0x1F64F
+        let symbolStart = 0x1F680
+        let symbolEnd = 0x1F6C5
+        
+        let emojiRange = 79
+        let symbolRange = 70
+        
+        let ascii = emojiStart +
+            Int(arc4random_uniform(UInt32(emojiRange)))
+        let emoji = UnicodeScalar(ascii)?.description
+        return emoji!
+    }
+    
+    private func updateCellView() -> Void
+    {
+        if (currentBucketItem != nil)
+        {
+            bucketItemSignature.text = currentBucketItem.itemAuthor
+            bucketItemText.text = currentBucketItem.itemContents
+        }
+        else
+        {
+            bucketItemSignature.text = "Author goes here"
+            bucketItemText.text = "Bucket item contents here"
+        }
+        bucketItemSymbol.text = randomEmoji()
+    }
+}
---
+++ DataProject/Model/BucketItem.swift
@@ -0,0 +1,21 @@
+//
+//  BucketItem.swift
+//  DataProject
+//
+//  Created by Porter, Tyler on 12/5/19.
+//  Copyright © 2019 CTEC. All rights reserved.
+//
+
+import Foundation
+
+public class BucketItem
+{
+    let itemContents : String
+    let itemAuthor : String
+    
+    init(contents : String, author : String)
+    {
+        itemContents = contents
+        itemAuthor = author
+    }
+}
